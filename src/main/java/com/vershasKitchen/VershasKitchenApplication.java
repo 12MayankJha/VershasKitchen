b38f701ff6dd70cc5f1c03ad3d072a1d58ed5669
MAYANK\mayank jha <mayank.jha1993>
2021-06-13 15:34:06 +0530

    Intial commit

---
+++ src/main/java/com/vershasKitchen/VershasKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.vershasKitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VershasKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VershasKitchenApplication.class, args);
+	}
+
+}
