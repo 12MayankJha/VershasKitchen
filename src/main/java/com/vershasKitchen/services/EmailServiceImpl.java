fed270148b007aff7815987f17e31a2267546caa
MAYANK\mayank jha <mayank.jha1993>
2021-07-05 09:01:41 +0530

    testing html classpath issue

--- src/main/java/com/vershasKitchen/services/EmailServiceImpl.java
+++ src/main/java/com/vershasKitchen/services/EmailServiceImpl.java
@@ -29,19 +29,19 @@ public class EmailServiceImpl implements EmailService {
 			helper.setTo(ClientHelper.VERSHASKITCHEN_EMAIL_ID);
 			helper.setSubject(ClientHelper.VERSHASKITCHEN_EMAIL_SUBJECT);
 
-			File resource = new ClassPathResource("ClientDetails.html").getFile();
-			String clientDetailsHtml = new String(Files.readAllBytes(resource.toPath()));
+//			File resource = new ClassPathResource("ClientDetails.html").getFile();
+//			String clientDetailsHtml = new String(Files.readAllBytes(resource.toPath()));
+//
+//			clientDetailsHtml = clientDetailsHtml
+//					.replace("{name}", details.getName())
+//					.replace("{phonenumber}", details.getPhoneNumber())
+//					.replace("{email}", details.getEmail())
+//					.replace("{message}", details.getMessage());
 
-			clientDetailsHtml = clientDetailsHtml
-					.replace("{name}", details.getName())
-					.replace("{phonenumber}", details.getPhoneNumber())
-					.replace("{email}", details.getEmail())
-					.replace("{message}", details.getMessage());
-
-			helper.setText(clientDetailsHtml, true);
+			helper.setText("Testimg", true);
 			emailSender.send(message);
 
-		} catch (IOException | MessagingException e) {
+		} catch ( MessagingException e) {
 			e.printStackTrace();
 		}
 
