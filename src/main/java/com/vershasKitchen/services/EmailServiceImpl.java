6882700a4ba05dff9a070cb30328af423e6a756b
MAYANK\mayank jha <mayank.jha1993>
2021-07-05 09:52:59 +0530

    adding Streams to for getting the html resource

--- src/main/java/com/vershasKitchen/services/EmailServiceImpl.java
+++ src/main/java/com/vershasKitchen/services/EmailServiceImpl.java
@@ -1,12 +1,13 @@
 package com.vershasKitchen.services;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -29,17 +30,18 @@ public class EmailServiceImpl implements EmailService {
 			helper.setTo(ClientHelper.VERSHASKITCHEN_EMAIL_ID);
 			helper.setSubject(ClientHelper.VERSHASKITCHEN_EMAIL_SUBJECT);
 
-			File resource = new ClassPathResource("/ClientDetails.html").getFile();
-			String clientDetailsHtml = new String(Files.readAllBytes(resource.toPath()));
-
-			clientDetailsHtml = clientDetailsHtml
-					.replace("{name}", details.getName())
-					.replace("{phonenumber}", details.getPhoneNumber())
-					.replace("{email}", details.getEmail())
-					.replace("{message}", details.getMessage());
-
-			helper.setText(clientDetailsHtml, true);
-			emailSender.send(message);
+			try (InputStream inputStream = getClass().getResourceAsStream("/ClientDetails.html");
+					BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+				String clientDetailsHtml = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+				clientDetailsHtml = clientDetailsHtml
+						.replace("{name}", details.getName())
+						.replace("{phonenumber}", details.getPhoneNumber())
+						.replace("{email}", details.getEmail())
+						.replace("{message}", details.getMessage());
+				
+				helper.setText(clientDetailsHtml, true);
+				emailSender.send(message);
+			}
 
 		} catch (IOException | MessagingException e) {
 			e.printStackTrace();
